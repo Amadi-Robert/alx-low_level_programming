a9d3d76e1cafc33143d41f97aa820115439ef547
Amadi-Robert <amadirobert18@gmail.com>
2022-12-08 04:15:29 -0800

    Size in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: 0 is (Success)
+ */
+int main(void)
+{
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+        printf("SIze of int: %d byte(s)\n", sizeof(int));
+	printf("Size of double: %d byte(s)\n", sizeof(double));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
+	printf("Size of long int: %d byte(s)\n", sizeof(long int));
+	return (0);
+}	

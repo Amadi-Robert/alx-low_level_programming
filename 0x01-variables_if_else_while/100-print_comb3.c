da229de5798caa9daad2ef9eab81dc02a5ec3f0f
Amadi-Robert <amadirobert18@gmail.com>
2022-12-10 03:17:56 -0800

    100-comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,22 +8,24 @@
 
 int main(void)
 {
-	int digit1, digit2;
+	int i, j;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (digit2 = 0; digit2 < 10; digit2++)
+		for (j = 1; j <= 9; j++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 9 && digit2 == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
+			if (j > i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }

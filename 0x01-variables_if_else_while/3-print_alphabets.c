5ba5f40cb25f9eb2295ebe2bc56b90a645761126
Amadi-Robert <amadirobert18@gmail.com>
2022-12-10 00:18:42 -0800

    3-alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Alawys 0 Success
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++);
+		putchar(ch);
+	for (ch = 'A'; ch <= 'A'; ch++);
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}

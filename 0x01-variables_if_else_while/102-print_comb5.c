725f5795980e4d3aeae50b763820910f8d2294d8
Amadi-Robert <amadirobert18@gmail.com>
2022-12-10 03:10:23 -0800

    102-comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,22 +7,25 @@
 
 int main(void)
 {
-	int num1, num2;
+	int i, j;
 
-	for (num1 = 0; num1 <= 98; num1++)
+	for (i = 0; i < 100; i++)
 	{
-		for (num2 = num1 + 1; num2 <= 98; num2++)
+		for (j = 0; j < 100; j++)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-
-			if (num1 == 98 && num2 == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
+			if (j > i)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');

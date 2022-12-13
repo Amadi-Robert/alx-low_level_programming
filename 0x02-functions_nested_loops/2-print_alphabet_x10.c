5ea9a9d9e35ea040cdf8b8a99a39246f9cf5126b
Amadi-Robert <amadirobert18@gmail.com>
2022-12-13 02:04:57 -0800

    2-alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,10 @@ void print_alphabet_x10(void)
 	int count;
 	char letter;
 
-	while (count++ < 9)
+	while (count < 9)
+		count++;
 
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-	_putchar('\n');
+		_putchar('\n');
 }

53d08ece909f3c9ecf0f8d6222d407a6418968df
Amadi-Robert <amadirobert18@gmail.com>
2022-12-13 15:41:42 -0800

    11-print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *print_to_98 - prints all numbers from input to 98
+ *@num: the unknown number
+ */
+
+void print_to_98(int num)
+{
+	while (num > 98)
+		printf("%d, ", num--);
+	printf("%d\n", num);
+}
+else
+{
+	while (num < 98)
+		printf("%d, ", num++);
+	printf("%d\n", num);
+}

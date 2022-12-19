786ef38242fc19f12dc136a6ebc502db3c3d5d68
Amadi-Robert <amadirobert18@gmail.com>
2022-12-19 02:56:56 -0800

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function that prints a spring followed by a new line
+ * @str: integer to be printed
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	_puts(str);
+}

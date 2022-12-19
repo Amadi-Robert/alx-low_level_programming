772dababd958d46bd568e13d969f174244d5bf7d
Amadi-Robert <amadirobert18@gmail.com>
2022-12-19 13:49:24 -0800

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,7 +3,7 @@
  * puts_half - prints half of a string
  *
  * @str: string to be printed
- * Return: 0
+ * Return: nothing
  */
 void puts_half(char *str)
 {
@@ -12,11 +12,17 @@ void puts_half(char *str)
 	while (str[string] != '\0')
 		string++;
 	if (string + 1 % 2 != '0')
+	{
 		n = (string - 1) / 2;
+	}
 	else
+	{
 		n = (string / 2);
 	n++;
+	}
 	for (string = n; str[string] != '\0'; string++)
+	{
 			_putchar(str[string]);
+	}
 			_putchar('\n');
 }

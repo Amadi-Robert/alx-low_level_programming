e5217d790f1cc7916a5e93b5d403ee7b0934a2e6
Amadi-Robert <amadirobert18@gmail.com>
2022-12-23 00:22:23 -0800

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,25 +4,20 @@
  *
  * @dest: string to be concatenation
  * @src: string to be concatenated
- * Return: 0
+ * Return: pointer to the resulting string dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int lengthA, lengthB;
+	int i, j;
 
-	lengthA = 0;
-	lengthB = 0;
+	i = 0;
+	j = 0;
 
-	while (*(dest + lengthA) != '\0')
-		lengthA++;
+	while (dest[j++])
+	i++;
+	for (j = 0; src[j]; j++)
+	dest[i++] = src[j];
 
-	while (*(src + lengthB) != '\0' && lengthA < 97)
-	{
-		*(dest + lengthA) = *(src + lengthB);
-		lengthA++;
-		lengthB++;
-	}
-	*(dest + lengthA) = '\0';
 	return (dest);
 }

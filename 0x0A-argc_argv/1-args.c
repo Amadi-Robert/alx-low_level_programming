dde59e89dc7976849f8cca16515990e422dd57ab
Amadi-Robert <amadirobert18@gmail.com>
2023-01-07 11:17:13 -0800

    2-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - prints number of arguments passed into it
+  * @argc: counts the number of arguments passed
+  * @argv: string of arguments passed
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+
+
+	return (0);
+}

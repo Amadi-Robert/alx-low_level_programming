049049a07362f7672defaaf576645703775cd62f
Amadi-Robert <amadirobert18@gmail.com>
2023-01-09 12:31:08 -0800

    2-str_cocat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,6 +19,14 @@ char *str_concat(char *s1, char *s2)
 
 	s = malloc(size * sizeof(char));
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	if (s == NULL)
 	{
 		return (NULL);

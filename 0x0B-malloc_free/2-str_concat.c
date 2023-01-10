1df692fcccdc1833d2abdc14b2c2f8df9ac942d9
Amadi-Robert <amadirobert18@gmail.com>
2023-01-10 07:20:02 -0800

    2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
+
 /**
   * str_concat - function that concatenates two strings
   * @s1: first string
@@ -10,36 +10,35 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, len1, len2, size;
-	char *s;
+	unsigned int i, j, len1, len2;
+	char *newtxt1, *newtxt2;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	size = len1 + len2 + 1;
-
-	s = malloc(size * sizeof(char));
+	i = 0;
+	j = 0;
+	len1 = 0;
+	len2 = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	if (s == NULL)
-	{
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+	newtxt1 = malloc(sizeof(char) * (len1 + len2 + 1));
+	newtxt2 = newtxt1;
+	if (newtxt1 == NULL)
 		return (NULL);
-	}
-
-	for (i = 0; i <= len1; i++)
+	while (i < len1)
 	{
-		s[i] = s1[i];
+		newtxt1[i] = s1[i];
+		i++;
 	}
-	for (i = 0; i < len2; i++)
+	while (j <= len2 + 1)
 	{
-		s[len1 + i] = s2[i];
+		newtxt1[i] = s2[j];
+		j++;
 	}
-	s[size - 1] = '\0';
-	return (s);
+	return (newtxt2);
 }

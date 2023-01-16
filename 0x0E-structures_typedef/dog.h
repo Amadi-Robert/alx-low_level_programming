54d4bdda3fa760e5f648ed5e5b5d7985514ecafe
Amadi-Robert <amadirobert18@gmail.com>
2023-01-16 06:22:38 -0800

    dog_h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,11 +6,17 @@
   * @age: age of the dog
   * @owner: person owning the dog
   */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} my_dog;
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif

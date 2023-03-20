c2f13d5050d6fde6e221f8b1fcfcb112cfb1605c
Amadi-Robert <amadirobert18@gmail.com>
2023-03-20 11:21:03 -0700

    Dynamic Library

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a spring followed by a new line
+ * @str: integer to be printed
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - computes absolute value of an integer
+ *@n: the int to be checked
+ *Return: the abs of the int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
